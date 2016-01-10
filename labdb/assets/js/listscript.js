b527826f425515bd64ccc5ca6e11cb1ffa2122f8
Christopher Yeh <chrisyeh96@gmail.com>
2016-01-10 15:34:06 -0800

    Made SURA LabDB real-time integrated with Google Sheets

--- labdb/assets/js/listscript.js
+++ labdb/assets/js/listscript.js
@@ -1,10 +1,59 @@
+/*****************
+ * Initialize List
+ *****************/
+
+// valueNames: class names for the different values of each list item
+// page: how many items that should be visible at the same time. Default 200
+// item: ID of item template element
 var options = {
-  valueNames: ['name', 'pi', 'department', 'goals', 'primary', 'secondary', 'website', 'notes'],
-  item: '<li><h3 class="name"></h3><p class="pi"></p><p class="department"></p><p class="goals"></p><p class="primary"></p><p class="secondary"></p><p class="website"></p><p class="notes"></p></li>'
+  valueNames: ['name', 'pi', 'category', 'department', 'goals', 'primary', 'secondary', 'website'],
+  page: 1000,
+  item: 'template'
 };
 
-var labsList;
+var labsList = new List('labs', options);
 
-$.getJSON( "assets/labdata/biology.json", function( data ) {
-  labsList = new List('labs', options, data);
+
+/***************************************
+ * Retrieve Data from Google Spreadsheet
+ ***************************************/
+
+var updateResults = function(error, options, response) {
+    console.log("Errors:", error);
+    var data = [];
+    for (var i=1; i<response["rows"].length; i++) {
+        data.push(response["rows"][i]["cells"]);
+    }
+    console.log("Total number of labs:", data.length);
+    labsList.add(data);
+
+    // make website links clickable
+    $("a.website").each( function() {
+      $( this ).attr("href", $( this ).text());
+    });
+}
+
+var params = {
+  url: 'https://docs.google.com/spreadsheets/d/11jJmYO_zn8LBk68KZWdsf0Uz_E7BQeL9AiB2gz6MSy0/edit#gid=547440635',
+  query: "select *",
+  callback: updateResults,
+  reset: true
+};
+
+sheetrock(params);
+
+
+/***********
+ * Filtering
+ ***********/
+
+$('#filter-category').change(function () {
+    var selection = this.value;
+    if (selection) {
+        labsList.filter(function(item) {
+          return (item.values().category == selection);
+        });
+    } else {
+        labsList.filter();
+    }
 });
